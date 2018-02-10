44279ac6f8352657c6aa1df224f71ca31efae32c
marck1995 <36246893+marck1995@users.noreply.github.com>
2018-02-10 11:09:09 -0600

    Add files via upload

---
+++ Pedido.php
@@ -0,0 +1,34 @@
+<?php
+if (!isset($_POST['email'])) {
+
+}else{
+  $mensaje="Mensaje del formulario de contacto de nnatali.com";
+  $mensaje.= "\nNombre: ". $_POST['nombre'];
+  $mensaje.= "\nEmail: ".$_POST['email'];
+  $mensaje.= "\nTelefono: ". $_POST['telefono'];
+  $mensaje.= "\nMensaje: \n".$_POST['mensaje'];
+  $destino= "tuemail@loquesea.com";
+  $remitente = $_POST['email'];
+  $asunto = "Mensaje enviado por: ".$_POST['nombre'];
+  mail($destino,$asunto,$mensaje,"FROM: $remitente");
+?>
+  <p><strong>Mensaje enviado.</strong></p>
+<?php
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
